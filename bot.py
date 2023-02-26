543a71be53ba657180463eee4fc6605086675233
juanrwx <juanrwx@outlook.com>
2023-02-26 18:01:55 -0300

    add get_usdt

--- bot.py
+++ bot.py
@@ -1,1 +1,25 @@
-#bot stocks
+import requests
+import json
+
+
+BELO_USDT = "https://criptoya.com/api/belo/usdt/ars/0.5"
+
+
+def get_mep():
+    pass
+
+def get_usdt(url):
+    req = requests.get(url).text
+    res = json.loads(req)
+    value_usdt = res.get('ask')
+    print("The usdt value is: " + str(value_usdt))
+
+
+def get_tsla():
+    pass
+
+
+
+
+
+get_usdt(BELO_USDT)
