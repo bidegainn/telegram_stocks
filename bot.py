d23b32b6fc77be03b7104da537b429ef57584183
juanrwx <juanrwx@outlook.com>
2023-02-26 18:43:18 -0300

    add get_mep

--- bot.py
+++ bot.py
@@ -2,17 +2,22 @@ import requests
 import json
 
 
-BELO_USDT = "https://criptoya.com/api/belo/usdt/ars/0.5"
+USDT = "https://criptoya.com/api/belo/usdt/ars/0.5"
+MEP = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
 
 
-def get_mep():
-    pass
 
 def get_usdt(url: str) -> str:
     req = requests.get(url).text
     res = json.loads(req)
     value_usdt = res.get('ask')
     print("The usdt value is: " + str(value_usdt))
+
+
+def get_mep(url: str) -> str:
+    req = requests.get(url).json()[4]
+    value_mep = req.get('casa').get('compra')
+    print("The USDMep value is: " + str(value_mep))
 
 
 def get_tsla():
@@ -22,4 +27,5 @@ def get_tsla():
 
 
 
-get_usdt(BELO_USDT)
+get_usdt(USDT)
+get_mep(MEP)
